bdbf1b94cb100c70017d9e96f819badaec17f58e
Elizabeth Mathew <elizabethmathew@Dineshs-MacBook-Pro.local>
2021-11-13 13:13:02 +0530

    AirCanvas commit

---
+++ chapter1.py
@@ -0,0 +1,76 @@
+import cv2.cv2 as cv2
+import numpy as np
+
+# READ WEBCAM #
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 130)  # brightness
+
+# orange green purple
+myColors = [[0, 187, 6, 149, 214, 255],
+            [38, 134, 68, 132, 214, 255],
+            [98, 109, 43, 142, 202, 202]]
+
+# BGR
+myColorValues = [[51, 153, 255],
+                 [0, 255, 0],
+                 [255, 0, 255]]
+
+myPoints = []
+
+
+# to detect color
+def findColor(img, myColors, myColorValues):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    count = 0
+    newPoints = []
+    for color in myColors:
+        lower = np.array(color[0:3])
+        upper = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        x, y = getContours(mask)
+        cv2.circle(imgResult, (x, y), 10, myColorValues[count], cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x, y, count])
+        count += 1
+    return newPoints
+
+    # cv2.imshow(str(color[0]),mask)
+
+
+# to create bounding box
+def getContours(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            # cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 4)
+            perimeter = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * perimeter, True)
+            x, y, w, h = cv2.boundingRect(approx)
+    return x + w // 2, y
+
+
+# to draw
+def drawOnCanvas(myPoints, myColorValues):
+    for point in myPoints:
+        cv2.circle(imgResult, (point[0], point[1]), 10, myColorValues[point[2]], cv2.FILLED)
+
+
+while True:
+    success, img = cap.read()
+    imgResult = img.copy()
+    newPoints = findColor(img, myColors, myColorValues)
+    if len(newPoints) != 0:
+        for newP in newPoints:
+            myPoints.append(newP)
+    if len(myPoints) != 0:
+        drawOnCanvas(myPoints, myColorValues)
+    # cv2.imshow("Output",img)
+    cv2.imshow("Contour", imgResult)
+    if cv2.waitKey(1) & 0xFF == ord('Q'):
+        break
